79d8ac8c84ec0394b7b20baebde82a039b25e03a
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-11 16:41:40 -0400

    Setting and Patching Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,23 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+
+    // **** NOT THE BEST APPROACH ****
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ""
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
   }
 
   // onSubmit(form: NgForm) {
